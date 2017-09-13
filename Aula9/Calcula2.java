5b621629bea5d0b21946ca4d36bf2a88e8963858
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2017-09-13 20:37:17 -0300

    Update Calcula2.java

--- Aula9/Calcula2.java
+++ Aula9/Calcula2.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Calcula2
  */
-@WebServlet("/Calcula3")
+@WebServlet("/Calcula2")
 public class Calcula2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
