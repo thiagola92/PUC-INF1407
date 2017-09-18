3a09881dc11e6c805283b5a5331b1ee7a7867fa6
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2017-09-18 20:27:49 -0300

    Update CalculaServlet.java

--- Aula10/outroExemplo/CalculaServlet.java
+++ Aula10/outroExemplo/CalculaServlet.java
@@ -1,6 +1,7 @@
 package pacote;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,39 @@ import javax.servlet.http.HttpServletResponse;
 public class CalculaServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// obter dados
+		String s0p1 = request.getParameter("operando1");
+		String s0p2 = request.getParameter("operando2");
+		String operacao = request.getParameter("operacao");
+		
+		// processar dados
+		double operando1 = Double.parseDouble(s0p1);
+		double operando2 = Double.parseDouble(s0p2);
+		double resultado = 0;
+		
+		if(operacao.equals("+")) {
+			resultado = operando1 + operando2;
+		} else if(operacao.equals("-")) {
+			resultado = operando1 - operando2;
+		} else if(operacao.equals("/")) {
+			resultado = operando1 / operando2;
+		} else if(operacao.equals("x")) {
+			resultado = operando1 * operando2;
+		} else {
+			// ERRO!!!
+			request.setAttribute("tipoErro", "operacao invalida");
+			request.getRequestDispatcher("../erros/erro.jsp").forward(request, response);
+			return;
+		}
+		
+		// encaminhar resultados para o visual
+		CalculaBean bean = new CalculaBean();
+		bean.setOp1(operando1);
+		bean.setOp2(operando2);
+		bean.setOperador(operacao);
+		bean.setResultado(resultado);
+		request.setAttribute("conta", bean);
+		request.getRequestDispatcher("calculadora/resultado.jsp").forward(request, response);
 		
 	}
 
