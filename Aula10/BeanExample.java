27952741e30aa05235fb17b078272977e5f56449
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2017-09-18 20:00:46 -0300

    Create BeanExample.java

---
+++ Aula10/BeanExample.java
@@ -0,0 +1,20 @@
+package pacote;
+
+public class BeanExample {
+	
+	private String nome;
+	
+	public BeanExample() {
+		super();
+		this.nome="thiago";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
