a8f39f9ba72460b1ea40457c9112483d7b3ca50e
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2017-09-18 20:09:20 -0300

    Create CalculaServlet.java

---
+++ Aula10/outroExemplo/CalculaServlet.java
@@ -0,0 +1,17 @@
+package pacote;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CalculaServlet")
+public class CalculaServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
