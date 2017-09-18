30ede48d9b71a0f7d7bd499749260389b4bdef0f
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2017-09-18 20:08:26 -0300

    Create CalculaBean.java

---
+++ Aula10/outroExemplo/CalculaBean.java
@@ -0,0 +1,32 @@
+package pacote;
+
+public class CalculaBean {
+	private double op1;
+	private double op2;
+	private String operador;
+	private double resultado;
+	public double getOp1() {
+		return op1;
+	}
+	public void setOp1(double op1) {
+		this.op1 = op1;
+	}
+	public double getOp2() {
+		return op2;
+	}
+	public void setOp2(double op2) {
+		this.op2 = op2;
+	}
+	public String getOperador() {
+		return operador;
+	}
+	public void setOperador(String operador) {
+		this.operador = operador;
+	}
+	public double getResultado() {
+		return resultado;
+	}
+	public void setResultado(double resultado) {
+		this.resultado = resultado;
+	}
+}
